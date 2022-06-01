ea91835cdfaf46b8195aa86005a0acb3556c0134
volllga <volllga@ya.ru>
2022-06-01 08:39:47 +0300

    add lesson05.js

---
+++ scrach.js
@@ -0,0 +1,17 @@
+const str = 'one, two, three';
+const str1 = 'something';
+
+console.log(str);
+console.log(str.split(', '));
+console.log(str1.split('').join('-'));
+
+
+const car = {
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 1969
+}
+
+for(let key in car){
+  console.log(key, car[key]);
+}
