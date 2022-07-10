f260cff43f99f6f7fbf9b7fda8d3591977b10e83
volllga <volllga@ya.ru>
2022-07-10 19:07:49 +0300

    delete dir lesson15

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,25 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es2021": true,
-
+        "jasmine": true
     },
+
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:jasmine/recommended"
     ],
+
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
     },
+
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "jasmine",
     ],
+
     "rules": {
       "semi": ["error", "always"],
       "quotes": ["error", "double"]
