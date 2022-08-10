5c990bd747a723228d8f3a40d4a84632101b8f84
volllga <volllga@ya.ru>
2022-08-10 09:19:45 +0300

    Third.spec.cy.js

--- cypress/cypress/e2e/Third.spec.cy.js
+++ cypress/cypress/e2e/Third.spec.cy.js
@@ -1,6 +1,11 @@
 require('cypress-xpath');
 import SecondBlock from '../objects/secondBlock';
+
 describe('First component of /example-4 page', () => {
+
+  Cypress.Commands.add('selectOption', (option) => {
+    cy.get('data-cy="box-3-dropdown"').select(option);
+  });
 
   before( () => {
     cy.visit('http://localhost:3000/example-4');
@@ -16,11 +21,30 @@ describe('First component of /example-4 page', () => {
     title.should('have.text', '.select()');
   });
 
-  Cypress.Commands.add('selectOption', (option) => {
-    cy.get('data-cy="box-3-dropdown"').select(option);
+  it('Nothing selected without selection', () => {
+    cy.get('[data-cy="box-3-selected-name"]').should('have.text', 'Nothing selected');
   });
 
-  cy.selectOption('');
+  it('Can select Option One ', () => {
+    cy.chooseOption('Option One');
+  });
 
+  it('Can select Option Two ', () => {
+    cy.chooseOption('Option Two');
+  });
+
+  it('Can select Option Three ', () => {
+    cy.chooseOption('Option Three');
+  });
+
+  it('Can change Option Three to Option One', () => {
+    cy.chooseOption('Option Three');
+    cy.chooseOption('Option One');
+  });
+
+  it('Can change Option One to Option Two', () => {
+    cy.chooseOption('Option One');
+    cy.chooseOption('Option Two');
+  });
 
 });
---
+++ cypress/cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('chooseOption',  (option) => {
+  cy.get('[data-cy="box-3-dropdown"]').select(option);
+  return cy.get('[data-cy="box-3-selected-name"]').should('have.text', option);
+});
+
+Cypress.Commands.add('choose',  (option) => {
+  return;
+});
