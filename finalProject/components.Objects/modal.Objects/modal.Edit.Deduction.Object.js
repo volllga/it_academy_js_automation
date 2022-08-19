fc6582e65a5ccfe9617aecbc8ca5ce8692ce814a
volllga <volllga@ya.ru>
2022-08-19 09:01:03 +0300

    final project delete modal edit deduction

--- finalProject/components.Objects/modal.Objects/modal.Edit.Deduction.Object.js
+++
@@ -1,9 +0,0 @@
-// const ModalObject = require("./modal.Object");
-//
-// class ModalEditDeductionObject extends ModalObject {
-//   get buttonRemove() {
-//     return $(".driver-statements-modal-payment__footer a");
-//   }
-// }
-//
-// module.exports = new ModalEditDeductionObject();
